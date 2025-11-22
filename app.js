87f7c91b648f966c472f13a837e27e42f3ea1014
Stalin-16 <stalinb.bca@gmail.com>
2025-11-22 10:28:52 +0530

    dbConfig changes

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-// app.use(
-//   helmet({
-//     contentSecurityPolicy: false,
-//   })
-// );
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 
 const expressLayouts = require("express-ejs-layouts");
 
